1e029eef8c50fb5918d9dad3141d00c902ecfb1c
dendev <zonicultra@gmail.com>
2026-04-02 09:42:13 +0800

    add Repository

---
+++ src/main/java/com/dendev/project_management/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dendev.project_management.repository;
+
+import com.dendev.project_management.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
